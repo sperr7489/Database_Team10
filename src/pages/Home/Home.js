c305131c2d57f52fec77bdbe956baa978d425095
zzbeen <sdhg12@naver.com>
2021-12-10 20:59:29 +0900

    backup

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
 import PersonElement from "../../Components/element/PersonElement.js";
-=======
-import React, { Fragment } from "react";
-import "../../Components/page.css"
->>>>>>> 15c48130475ba3836c4432066792a869e692eba6
 
-import React, { Fragment } from "react";
+import React from "react";
 import "../../Components/Navbar/page.css";
 
 const Home = () => {
